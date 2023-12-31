a273b1b697a3a73c250bf1eec71c926bb7a978d6
nuuuwan <nuuuwan@gmail.com>
2023-12-31 09:52:18 +0530

    minor

--- src/utils_ai/AIImage.py
+++ src/utils_ai/AIImage.py
@@ -11,19 +11,16 @@ class AIImage:
     DEFAULT_N_IMAGES = 1
     DEFAULT_SIZE = '1024x1024'
 
-    def draw(
-        self, description: str, image_path: str, size: str = None
-    ) -> str:
-        size = size or AIImage.DEFAULT_SIZE
-
+    def draw(self, description: str, image_path: str) -> str:
         tic = time.perf_counter()
         response = openai.Image.create(
-            prompt=description, n=AIImage.DEFAULT_N_IMAGES, size=size
+            prompt=description,
+            n=AIImage.DEFAULT_N_IMAGES,
+            size=AIImage.DEFAULT_SIZE,
         )
         image_url = response['data'][0]['url']
-        log.debug(f'Generated "{description}" -> {image_url}')
         WWW.download_binary(image_url, image_path)
         toc = time.perf_counter()
         log.info(
-            f'Downloaded "{image_url}" -> {image_path} ({toc - tic:0.4f}s)'
+            f'Downloaded "{description}" to {image_path} ({toc - tic:0.4f}s)'
         )
