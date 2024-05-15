e70fd56f7474b6ef7b895b630519039809ee4b9a
nuuuwan <nuuuwan@gmail.com>
2024-05-15 11:17:28 +0530

    Split run

--- src/utils_ai/apps/ChatApp.py
+++ src/utils_ai/apps/ChatApp.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from utils_base import Console, File, Log
 
@@ -9,35 +10,43 @@ log = Log('ChatApp')
 
 class ChatApp:
     @staticmethod
-    def run(provider_name: str, profile_path: str):
-        ai = ProviderFactory.from_name(provider_name)
-        print('')
-        print(Console.note(f'[{ai.NAME}/{ai.MODEL}]'))
-        print('')
-
+    def set_profile(ai, profile_path: str):
         if os.path.exists(profile_path):
             profile_content = File(profile_path).read()
             ai.set_profile(profile_content)
             print(Console.note(f'{profile_path}'))
             print('')
 
-        while True:
-            message = input('>> ')
-            print('')
-            if message in ['quit', 'exit', 'q', 'x', 'exit()', 'quit()']:
-                print(Console.note('Bye!'))
-                print()
-                break
+    @staticmethod
+    def run_iter(ai):
+        message = input('>> ')
+        print('')
+        if message in ['quit', 'exit', 'q', 'x', 'exit()', 'quit()']:
+            print(Console.note('Bye!'))
+            print()
+            sys.exit(0)
 
-            if message.lower().startswith('draw:'):
-                image_path = ai.draw(message)
-                if image_path:
-                    print(Console.note(image_path))
-                    print('')
-                    os.startfile(image_path)
-                continue
-
-            reply = ai.chat(message)
-            if reply:
-                print(Console.note(reply))
+        if message.lower().startswith('draw:'):
+            image_path = ai.draw(message)
+            if image_path:
+                print(Console.note(image_path))
                 print('')
+                os.startfile(image_path)
+            return
+
+        reply = ai.chat(message)
+        if reply:
+            print(Console.note(reply))
+            print('')
+
+    @staticmethod
+    def run(provider_name: str, profile_path: str):
+        ai = ProviderFactory.from_name(provider_name)
+        print('')
+        print(Console.note(f'[{ai.NAME}/{ai.MODEL}]'))
+        print('')
+
+        ChatApp.set_profile(ai, profile_path)
+
+        while True:
+            ChatApp.run_iter(ai)
