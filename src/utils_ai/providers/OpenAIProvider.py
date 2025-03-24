4869bd2b6522c025d6fc68c8d871ac8b611a697d
nuuuwan <nuuuwan@gmail.com>
2025-03-24 10:15:15 +0530

    Fixed bug in OpenAIProvider

--- src/utils_ai/providers/OpenAIProvider.py
+++ src/utils_ai/providers/OpenAIProvider.py
@@ -6,27 +6,26 @@ from utils_ai.generic_ai import GenericAI
 
 
 class OpenAIProvider(GenericAI):
-    NAME = 'OpenAI'
-    MODEL = 'gpt-4'
-    IMAGE_MODEL = 'dall-e-3'
+    NAME = "OpenAI"
+    MODEL = "gpt-4"
+    IMAGE_MODEL = "dall-e-3"
 
     def __init__(self):
         GenericAI.__init__(self)
 
-        self.client = openai.OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+        openai.api_key = os.getenv("OPENAI_API_KEY")
 
     def get_chat_reply(self, messages: list) -> str:
-        response = self.client.chat.completions.create(
+        response = openai.ChatCompletion.create(
             model=OpenAIProvider.MODEL, messages=messages
         )
-        return response.choices[0].message.content
+        return response.choices[0].message["content"]
 
     def get_image_url(self, prompt: str) -> str:
-        response = self.client.images.generate(
+        response = openai.Image.create(
             model=OpenAIProvider.IMAGE_MODEL,
             prompt=prompt,
             size="1024x1024",
-            quality="standard",
             n=1,
         )
-        return response.data[0].url
+        return response.data[0]["url"]
