1a41651d2922065f5d86a89c419b1e5c92473406
nuuuwan <nuuuwan@gmail.com>
2025-05-10 21:52:03 +0530

    show options in commandline

--- workflows/chat.py
+++ workflows/chat.py
@@ -2,7 +2,10 @@ import sys
 
 from utils_ai import ChatApp
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python chat.py <OpenAI|MistralAI> <ai_profiles/*.txt>")
+        sys.exit(1)
     provider_name = sys.argv[1]
     profile_path = sys.argv[2]
     ChatApp.run(provider_name, profile_path)
