43fb229d5a42a06be2f6d582e70eb33daf9dcdf0
nuuuwan <nuuuwan@gmail.com>
2025-05-10 22:02:13 +0530

    Updated to new OpenAI

--- src/utils_ai/providers/OpenAIProvider.py
+++ src/utils_ai/providers/OpenAIProvider.py
@@ -1,31 +1,30 @@
 import os
-
-import openai
+from openai import OpenAI
 
 from utils_ai.generic_ai import GenericAI
 
 
 class OpenAIProvider(GenericAI):
     NAME = "OpenAI"
-    MODEL = "gpt-4"
+    MODEL = "gpt-4o"
     IMAGE_MODEL = "dall-e-3"
 
     def __init__(self):
-        GenericAI.__init__(self)
-
-        openai.api_key = os.getenv("OPENAI_API_KEY")
+        super().__init__()
+        self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
     def get_chat_reply(self, messages: list) -> str:
-        response = openai.ChatCompletion.create(
-            model=OpenAIProvider.MODEL, messages=messages
+        response = self.client.chat.completions.create(
+            model=self.MODEL,
+            messages=messages,
         )
-        return response.choices[0].message["content"]
+        return response.choices[0].message.content
 
     def get_image_url(self, prompt: str) -> str:
-        response = openai.Image.create(
-            model=OpenAIProvider.IMAGE_MODEL,
+        response = self.client.images.generate(
+            model=self.IMAGE_MODEL,
             prompt=prompt,
             size="1024x1024",
             n=1,
         )
-        return response.data[0]["url"]
+        return response.data[0].url
